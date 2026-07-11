bcbd2de691942ab8886cf24e0b603afb664e1c3d
MatheusBorgesBS <matheus.borges.bs@outlook.com>
2026-07-11 11:09:24 -0300

    Implementação Inicial do Dijkstra

---
+++ Dijkstra.py
@@ -0,0 +1,64 @@
+### Testes
+
+def dijkstra(grafo, origem):
+    inf = float('inf')
+    distancias = {
+        no : inf for no in grafo
+    }
+    antecessores = {
+        no: None for no in grafo
+    }
+    visitados = set()
+
+    distancias[origem] = 0
+
+    while len(visitados) < len(grafo):
+        no_atual = None
+        menor_dist = inf
+
+        for no in grafo:
+            if no not in visitados and distancias[no] < menor_dist:
+                no_atual = no
+                menor_dist = distancias[no]
+        if no_atual is None:
+            break
+
+
+        visitados.add(no_atual)
+
+        for vizinho, peso in grafo[no_atual].items():
+            nova_dist = distancias[no_atual] + peso
+            if nova_dist < distancias[vizinho]:
+                distancias[vizinho] = nova_dist
+                antecessores[vizinho] = no_atual
+    
+    return distancias,antecessores
+
+
+
+
+nodes = {
+    'S': {'A': 6,'D': 3},
+    'A': {'B': 3 , 'D': 1},
+    'D': {'A': 2, 'B': 7, 'C': 1},
+    'B': {'C': 5},
+    'C': {} 
+}
+
+
+nodes = {
+    "S": {"A": 6, "D": 3},
+    "A": {"B": 3, "D": 1},
+    "D": {"A": 2, "B": 7, "C": 1},
+    "B": {"C": 5},
+    "C": {}
+}
+
+distancias, antecessores = dijkstra(nodes, "S")
+
+print("Distâncias:")
+print(distancias)
+
+print("Antecessores:")
+print(antecessores)
+
